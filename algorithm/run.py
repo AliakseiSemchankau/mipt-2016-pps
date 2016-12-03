a0308d0360e912058674dcd744ac7a7a7317dfcc
lena <anyusheva@phystech.com>
2016-12-04 02:53:43 +0300

    add exception handling to algorithm

--- algorithm/run.py
+++ algorithm/run.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, abort
 from order_scheduler_pb2 import *
 from computing_system_scheduler_pb2 import *
+import sys
 from json import loads
 from sys import argv
 import threading
@@ -11,8 +12,6 @@ import requests
 app = Flask(__name__)
 
 ALGORITHM_ADDRESS=''
-ORDER_ADDRESS=''
-SYSTEM_ADDRESS=''
 SCHEDULER_ADDRESS=''
 
 pending_orders = []
@@ -28,29 +27,28 @@ def addTask():
 
 def sendTasks():
     while True:
-        while len(pending_orders) == 0:
-            time.sleep(random.randint(1, 2))
-        order = pending_orders.pop()
-        order_id = order.order_id
-        sub_tasks = order.task.sub_tasks
-        cluster_id = random.randint(1, 10)
-        for sub_task in sub_tasks:
-            sub_task_order = SubTaskOrder()
-            sub_task_order.order_id = order_id
-            sub_task_order.sub_task = sub_task
-            sub_task_order.cluster_id = cluster_id
-            resp = requests.post(SCHEDULER_ADDRESS + '/send_subtask', data=sub_task_order.SerializeToString())
-        if not resp.ok:
-            abort(res.code)
-        return resp.content
+        try:
+            while len(pending_orders) == 0:
+                time.sleep(random.randint(1, 2))
+            order = pending_orders.pop()
+            order_id = order.order_id
+            sub_tasks = order.task.sub_tasks
+            cluster_id = random.randint(1, 10)
+            for sub_task in sub_tasks:
+                sub_task_order = SubTaskOrder()
+                sub_task_order.order_id = order_id
+                sub_task_order.sub_task.CopyFrom(sub_task)
+                sub_task_order.cluster_id = cluster_id
+                resp = requests.post(SCHEDULER_ADDRESS + '/send_subtask', data=sub_task_order.SerializeToString())
+            return resp.content
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
     conf = loads(open(argv[1]).read())
     ALGORITHM_ADDRESS = conf['algorithm_address']
-    ORDER_ADDRESS = conf['order_address']
-    SYSTEM_ADDRESS = conf['system_address']
-    SCHEDULER_ADDRESS = conf['scheduler_address']
+    SCHEDULER_ADDRESS = 'http://' + conf['scheduler_address']
     addr = ALGORITHM_ADDRESS.split(':')
     conf = {'host': addr[0]}
     if len(addr) > 1:
