f3e7e098c3a5867c5466bc9a96fe9ee13924d0e2
Mike <surinmike@gmail.com>
2016-12-04 13:22:12 +0300

    revert pickle->protobuf

--- algorithm/run.py
+++ algorithm/run.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, abort
-# from order_scheduler_pb2 import *
-# from computing_system_scheduler_pb2 import *
+from order_scheduler_pb2 import *
+from computing_system_scheduler_pb2 import *
 import sys
 from json import loads
 from sys import argv
@@ -9,18 +9,6 @@ import random
 import time
 from uuid import uuid4 as uuid
 import requests
-import pickle
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
-
-import order_scheduler
-import computer_system_scheduler
-
-from order_scheduler import Order
-from computer_system_scheduler import SubTaskOrder
-
 app = Flask(__name__)
 
 ALGORITHM_ADDRESS=''
@@ -31,7 +19,8 @@ pending_orders = []
 
 @app.route("/add_task", methods=['POST'])
 def addTask():
-    order = pickle.loads(request.get_data())
+    order = Order()
+    order.ParseFromString(request.get_data())
     pending_orders.append(order)
     return order.order_id
 
@@ -46,15 +35,11 @@ def sendTasks():
             sub_tasks = order.task.sub_tasks
             cluster_id = random.randint(1, 10)
             for sub_task in sub_tasks:
-
-                sub_task_order = SubTaskOrder(order_id, sub_task, cluster_id)
-
-                # sub_task_order = SubTaskOrder()
-                # sub_task_order.order_id = order_id
-                # sub_task_order.sub_task.CopyFrom(sub_task)
-                # sub_task_order.cluster_id = cluster_id
-
-                resp = requests.post(SCHEDULER_ADDRESS + '/send_subtask', data=pickle.dumps(sub_task_order))
+                sub_task_order = SubTaskOrder()
+                sub_task_order.order_id = order_id
+                sub_task_order.sub_task.CopyFrom(sub_task)
+                sub_task_order.cluster_id = cluster_id
+                resp = requests.post(SCHEDULER_ADDRESS + '/send_subtask', data=sub_task_order.SerializeToString())
             return resp.content
         except Exception as e:
             print(e)
--- scheduler/run.py
+++ scheduler/run.py
@@ -1,20 +1,11 @@
 from flask import Flask, request, abort
-# from order_scheduler_pb2 import *
-# from computing_system_scheduler_pb2 import *
+from order_scheduler_pb2 import *
+from computing_system_scheduler_pb2 import *
 from json import loads
 from sys import argv
 from uuid import uuid4 as uuid
 import requests
 app = Flask(__name__)
-
-import pickle
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
-
-import order_scheduler
-from order_scheduler import Order
 
 ALGORITHM_ADDRESS=''
 ORDER_ADDRESS=''
@@ -24,7 +15,8 @@ SCHEDULER_ADDRESS=''
 @app.route("/add_task", methods=['POST'])
 def addTask():
     order_id = str(uuid())
-    order = pickle.loads(request.get_data())
+    order = Order()
+    order.ParseFromString(request.get_data())
     order.order_id = order_id
     resp = requests.post(ALGORITHM_ADDRESS + '/add_task', data=order.SerializeToString())
     if not resp.ok:
@@ -59,9 +51,9 @@ def onSubTaskCompletion():
 
 if __name__ == '__main__':
     conf = loads(open(argv[1]).read())
-    ALGORITHM_ADDRESS = 'http://' + conf['algorithm_address']
-    ORDER_ADDRESS = 'http://' + conf['order_address']
-    SYSTEM_ADDRESS = 'http://' + conf['system_address']
+    ALGORITHM_ADDRESS = conf['algorithm_address']
+    ORDER_ADDRESS = conf['order_address']
+    SYSTEM_ADDRESS = conf['system_address']
     SCHEDULER_ADDRESS = conf['scheduler_address']
     addr = SCHEDULER_ADDRESS.split(':')
     conf = {'host': addr[0]}
