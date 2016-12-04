bbb3f9ec4c2483eefe9df43525ba94a4e8ccdff2
Семченков Алексей Сергеевич <leshaxim@mail.ru>
2016-12-04 18:01:49 +0300

    add get_result to web-interface

--- order/flask/run.py
+++ order/flask/run.py
@@ -18,6 +18,9 @@ class OrderForm(Form):
     processors_count = IntegerField('processors_count', validators=[Required()])
     time_durability = IntegerField('time_durability', validators=[Required()])
 
+class ResultForm(Form):
+	order_id = TextField('order_id', validators=[Required()])
+
 def send_order(subtasks_urls, processors_count, time_durability):
     task = Task()
     for url in subtasks_urls:
@@ -35,23 +38,46 @@ def send_order(subtasks_urls, processors_count, time_durability):
 
     return resp.content
 
+def get_result(order_id):
+
+	resp = requests.get(SCHEDULER_ADDRESS + '/get/' + order_id)
+	result = StatusMsg()
+	result.ParseFromString(resp.get_data())
+
+	return result
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
 
     order_form = OrderForm()
+    result_form = ResultForm()
 
-    kwargs = {'order_form': order_form}
+    kwargs = {'order_form': order_form, 'result_form': result_form}
 
     if order_form.validate_on_submit():
-
         sub_tasks_urls = str(order_form.sub_tasks_urls.data).split(' ')
         processors_count = int(order_form.processors_count.data)
         time_durability = int(order_form.time_durability.data)
-
         order_id = send_order(sub_tasks_urls, processors_count, time_durability)
-
         kwargs['order_id'] = order_id
+        return render_template("index.html", **kwargs)
+
+    if result_form.validate_on_submit():
+        order_id = str(result_form.order_id.data)
+        result = get_result(order_id)
+        if result.status == 0:
+        	kwargs['task_status'] = 'DONE'
+        if result.status == 1:
+            kwargs['task_status'] = 'UNDONE'
+        if result.status == 2:
+            kwargs['task_status'] = 'FAILED'
+
+        if result.status == 0:
+        	kwargs['task_result'] = result.result
+        else:
+        	kwargs['task_result'] = 'Not available'
 
         return render_template("index.html", **kwargs)
 
